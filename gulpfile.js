c846bd83f80aa31191dee8218133979929ce19d6
HADMARINE <tsx@kakao.com>
2021-07-15 12:33:45 +0900

    Update build sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var fs = require('fs');
 
-gulp.task('build', function() {
-    var merge = require('merge2');
-    var tsProject = ts.createProject('tsconfig.json');
+gulp.task('build', function () {
+  if (fs.existsSync('dist')) {
+    fs.rmdirSync('dist', { recursive: true });
+  }
 
-    var tsResult = tsProject.src()
-        .pipe(tsProject());
+  var merge = require('merge2');
+  var tsProject = ts.createProject('tsconfig.json');
 
-    return merge([
-        tsResult.dts.pipe(gulp.dest(tsProject.config.compilerOptions.outDir)),
-        tsResult.js.pipe(gulp.dest(tsProject.config.compilerOptions.outDir))
-    ]);
+  var tsResult = tsProject.src().pipe(tsProject());
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest(tsProject.config.compilerOptions.outDir)),
+    tsResult.js.pipe(gulp.dest(tsProject.config.compilerOptions.outDir)),
+  ]);
 });
